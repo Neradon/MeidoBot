8c1d31ab634182df02946ec4bb1b476d2220d515
Willem König <526125@edu.rocmn.nl>
2018-11-30 15:18:41 +0100

    Update BTC.py
    optimizing code

--- cogs/BTC.py
+++ cogs/BTC.py
@@ -1,9 +1,6 @@
-import json
 import discord
 import requests
 from discord.ext import commands
-
-urll = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
 
 class BTC:
 	def __init__(self, client):
@@ -11,10 +8,9 @@ class BTC:
 		
 	@commands.command()
 	async def BTC(self, ctx):
-		data = requests.get(urll).json()
+		data = requests.get('https://api.coindesk.com/v1/bpi/currentprice/BTC.json').json()
 		cur = data['bpi']['USD']['rate']
-		msg = str(cur)+" USD"
-		await ctx.send(msg)
+		await ctx.send(str(cur)+" USD")
 
 def setup(client):
 	client.add_cog(BTC(client))
