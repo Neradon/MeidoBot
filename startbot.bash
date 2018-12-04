5c0180e46c1b0946e26a7a01dcb549b72d48c583
Willem König <526125@edu.rocmn.nl>
2018-12-04 01:48:45 +0100

    Update startbot.bash

--- startbot.bash
+++ startbot.bash
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
 while true; do
-<<<<<<< HEAD
-    git pull git@github.com:LSparky/MeidoBot.git
-=======
     git pull git://github.com/LSparky/MeidoBot.git
     echo "waiting 4 seconds"
     sleep 4
->>>>>>> ffc2dc9... Update startbot.bash
     ./bot.py
     for i in {5..1}; do
         echo “Restarting in $i”
