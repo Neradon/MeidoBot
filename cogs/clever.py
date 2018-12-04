fbaff6eeabb7fd3f84235d4fac9e7b433b53f2a6
Willem König <526125@edu.rocmnl.nl>
2018-12-04 09:45:19 +0100

    Fixed cleverbot

--- cogs/clever.py
+++ cogs/clever.py
@@ -6,30 +6,29 @@ from discord.ext import commands
 from cleverwrap import CleverWrap
 
 with open('token.json') as token_file:
-	data = json.load(token_file)
+    data = json.load(token_file)
 clevertoken = data['clevertoken']
 
 cw = CleverWrap(clevertoken)
 
 class clever:
-	def __init__(self, client):
-		self.client = client
+    def __init__(self, client):
+        self.client = client
 
-	@commands.command(aliases=["cb"])
-	async def clever(self, ctx, *args):
-		arg1 = ("{} " * len(args)).format(*args)
-		print(arg1)
-		await ctx.send(cw.say(arg1))
+    @commands.command(aliases=["cb"])
+    async def clever(self, ctx, *args):
+        arg1 = ("{} " * len(args)).format(*args)
+        await ctx.send(cw.say(arg1))
 
-	@clever.error
-	async def clever_error(self, ctx, error):
-		if isinstance(error, commands.MissingRequiredArgument):
-			await ctx.send("Please enter a message with quotation marks around it!")
+    @clever.error
+    async def clever_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Please enter a message with quotation marks around it!")
 
-	@commands.command(aliases=["cb_reset"])
-	async def clever_reset(self, ctx):
-		cw.reset()
-		await ctx.send("Reset conversation!")
+    @commands.command(aliases=["cb_reset"])
+    async def clever_reset(self, ctx):
+        cw.reset()
+        await ctx.send("Reset conversation!")
 
 def setup(client):
-	client.add_cog(clever(client))
+    client.add_cog(clever(client))
