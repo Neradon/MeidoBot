ac2393b8d9c9fcc5e2369708d6300653402f712a
Neradon <neradon@neradonien.de>
2019-04-18 20:26:50 +0200

    Fix 4

--- cogs/voice.py
+++ cogs/voice.py
@@ -3,10 +3,11 @@ import re
 from discord.ext import commands
 
 
-class voice():
+class voice(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @commands.Cog.listener()
     async def on_message(self, message):
         print(str(message.author)+" - "+str(message.channel.id))
         await self.client.process_commands(message)
