0fb80e48358bdd7f7e7bdcd3ae943cddc327c4d2
Neradon <neradon@neradonien.de>
2019-04-18 20:55:33 +0200

    Fix 9

--- cogs/voice.py
+++ cogs/voice.py
@@ -10,19 +10,22 @@ class voice(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         safe = True
+        report = "STOP"
         if message.channel.id == 460913402458537985:
             safe = False
+            report = "https://i.imgur.com/eN4ea9f.gif"
             if message.author.voice is not None:
                 if message.author.voice.channel.id == 459425873234362369:
                     safe = True
         if message.channel.id == 528330541888700418:
             safe = False
+            report = "https://i.imgur.com/hvoixCS.gif"
             if message.author.voice is not None:
                 if message.author.voice.channel.id == 528329844044333058:
                     safe = True
         if safe is False:
             await message.delete()
-            await message.author.send("STOP")
+            await message.author.send(str(report))
         return
 
 
