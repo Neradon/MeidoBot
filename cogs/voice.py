e455cab83d6217b550b9db3556bb2b3484694d6b
Neradon <neradon@neradonien.de>
2019-04-18 20:51:00 +0200

    Fix 8

--- cogs/voice.py
+++ cogs/voice.py
@@ -12,16 +12,17 @@ class voice(commands.Cog):
         safe = True
         if message.channel.id == 460913402458537985:
             safe = False
-            if message.author.voice.channel is not None:
+            if message.author.voice is not None:
                 if message.author.voice.channel.id == 459425873234362369:
                     safe = True
         if message.channel.id == 528330541888700418:
             safe = False
-            if message.author.voice.channel is not None:
+            if message.author.voice is not None:
                 if message.author.voice.channel.id == 528329844044333058:
                     safe = True
         if safe is False:
             await message.delete()
+            await message.author.send("STOP")
         return
 
 
